d23ebbd9246be4a703697e7942675510e1224a74
bomimee <158027966+bomimee@users.noreply.github.com>
2024-07-06 12:02:16 +0900

    Update Result.js

--- src/app/job/test/result/Result.js
+++ src/app/job/test/result/Result.js
@@ -44,15 +44,15 @@ function PageContent() {
     index,
   }) => {
     const RADIAN = Math.PI / 180;
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.3;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.6;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) -50;
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
     return (
       <text
         x={x}
         y={y}
         fill="white"
-        textAnchor={x > cx ? "start" : "end"}
+        //textAnchor={x > cx ? "start" : "end"}
         dominantBaseline="central"
       >
         {`${chartData[index].name}: ${(percent * 100).toFixed(0)}%`}
