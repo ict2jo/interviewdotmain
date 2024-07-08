621abdf7fb18a075780b114a0ae9675a013abd20
Jun Hyeong Roh <union735@gmail.com>
2024-07-08 15:46:09 +0900

    병합 후 오류수정

--- src/stores/MenuStore.js
+++ src/stores/MenuStore.js
@@ -1,49 +1,34 @@
 import { makeAutoObservable } from "mobx";
 
 class MenuStore {
-  selectedMenu = "main";
+
+  selectedMenu = localStorage.getItem("selectedMenu") || "main";
   isAuthenticated = false;
   token = null;
   selectedResumeData = [];
 
   constructor() {
     makeAutoObservable(this);
-
-    // 클라이언트 사이드에서만 실행
-    if (typeof window !== "undefined") {
-      this.initializeFromLocalStorage();
-    }
   }
 
   // 메뉴 변경
   setSelectedMenu(menu) {
     this.selectedMenu = menu;
-    if (typeof window !== "undefined") {
-      localStorage.setItem("selectedMenu", menu);
-    }
+    localStorage.setItem("selectedMenu", menu);
   }
 
   setInquiryList(inquiryList) {
     this.inquiryList = inquiryList;
-    if (typeof window !== "undefined") {
-      localStorage.setItem("inquiryList", inquiryList);
-    }
+    localStorage.setItem("inquiryList", inquiryList);
   }
 
-  // 이력서 데이터 설정
+// 이력서 데이터 설정
   setSelectedResumeData(selectedResumeData) {
     this.selectedResumeData = selectedResumeData;
-    if (typeof window !== "undefined") {
-      localStorage.setItem("selectedResumeData", selectedResumeData);
-    }
+    localStorage.setItem("selectedResumeData", selectedResumeData);
   }
 
-  // 클라이언트 사이드에서만 localStorage 값을 초기화하는 메서드
-  initializeFromLocalStorage() {
-    this.selectedMenu = localStorage.getItem("selectedMenu") || "main";
-    this.inquiryList = localStorage.getItem("inquiryList") || [];
-    this.selectedResumeData = JSON.parse(localStorage.getItem("selectedResumeData")) || [];
-  }
+
 }
 
 const menuStore = new MenuStore();
